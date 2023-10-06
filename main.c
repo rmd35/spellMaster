827382d4d3b0f31e5dd5481e3cf97217956751cc
Rola <144335560+rmd35@users.noreply.github.com>
2023-10-06 12:37:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,34 +6,35 @@ void displayEffect() {
     printf("Stop! Who dares to venture into this land!?\n");
     sleep(3);
     printf("Well, well.\n"); 
-    printf("It seems you are no muggle after all! In that case, allow the Marauders to present: \n");
     sleep(2);
+    printf("It seems you are no muggle after all! In that case, allow the Marauders to present: \n");
+    sleep(3);
     printf("Hogwarts School of Witchcraft and Wizardry!\n");
-    sleep(1);
+    sleep(3);
     printf("Tonight, the grand hall is filled with an air of anticipation...\n");
-    sleep(1);
+    sleep(3);
     printf("Students from all four houses have gathered.\n");
-    sleep(1);
+    sleep(3);
     printf("Even the paintings seem alert.\n");   
 }
 void displayStart(char name1[], char name2[]) {
     printf("The Grandmaster Dumbledore steps forward and announces:\n\n");
-    sleep(2);
+    sleep(3);
     printf("'Tonight, we shall witness a duel of spells!'\n\n");
-    sleep(2);
+    sleep(3);
     printf("%s takes his position, ready to cast the first spell...\n\n", name1);
-    sleep(1);
+    sleep(3);
     printf("%s prepares to counter...\n\n", name2);
     printf("Wands out... \n\n");
-    sleep(2);
+    sleep(3);
     printf("3... \n");
     sleep(3);
     printf("2... \n");
-    sleep(3);
-    printf("1... \n");
     sleep(2);
-    printf("The Spell Master competition begins now! DUEL!\n\n");
+    printf("1... \n");
     sleep(1);
+    printf("The Spell Master competition begins now! DUEL!\n\n");
+    sleep(2);
 }
 int wordsStartingWithChar(char *words[], char c, int sizeOfArray) {
     for (int i = 0; i < sizeOfArray; i++) {
@@ -42,7 +43,7 @@ int wordsStartingWithChar(char *words[], char c, int sizeOfArray) {
     }
     return -1;
 }
-int isFound(const char *word, const char *array[], int size) {
+int isFound(char word[], char *array[], int size) {
     for (int i = 0; i < size; i++) {
         if (strcmp(word, array[i]) == 0) {
             return 1;
@@ -57,7 +58,7 @@ int sameLetter(char* oldWord, char* newWord){
 char lastChar= oldWord[strlen(oldWord)-1];
 char firstchar= newWord[0];
 if(firstchar==lastChar){
-    printf("slay");
+    //printf("slay");
     return 1;
 }
 return 0;
@@ -67,7 +68,9 @@ void printWords(char *words[], int sizeOfArray) {
          printf("%-20s", words[i]);
         if ((i + 1) % 5 == 0){
             printf("\n");
-    }}
+        }
+    }
+    printf("\n\n");
 
 }
 int main() {
@@ -113,12 +116,12 @@ int main() {
     printf("Powerful wizards who ventured thus far, tell us what you are called! \n");
     char player1[50];
     char player2[50];
-    scanf("%s",&player1);
-    scanf("%s", &player2);
+    scanf("%s", player1);
+    scanf("%s", player2);
     int random = rand() % 2;   // toss the coin 
     printf("The fates have conspired, the wands have been chosen, and now, it's time to decide who will cast the first spell...\n");
+    sleep(2);
     printf("let us toss a galleon and see what the laws of arithmancy pick!");
-    printf("\n %d",random);
     char Playing[50];
     if (random == 0) {
         printf("By the powers of the Elder Wand, %s shall commence this duel!\n", player1);
@@ -130,22 +133,37 @@ int main() {
         strcpy(Playing, player2);
     }
     char **chosenWords = (char **)malloc(wordCount * sizeof(char *));
-    chosenWords[wordCount];
+    if (chosenWords == NULL) {
+        printf("Memory allocation error for chosenWords\n");
+        free(chosenWords); // Clean up previous memory allocation
+        return 1;
+    }
     int turnCount = 0;
-    printf("What is your first move? \n");
     printWords(words, wordCount);
     char retaliate[80];
     do {
         printf("What is your next move?\n");
-        scanf("%s", &retaliate);
-        chosenWords[turnCount] = retaliate;
+        scanf("%s", retaliate);
+        chosenWords[turnCount] = (char *)malloc((strlen(retaliate) + 1) * sizeof(char));
+    if (chosenWords[turnCount] == NULL) {
+        printf("Memory allocation error for chosenWords[%d]\n", turnCount);
+        // Clean up memory and exit
+        for (int i = 0; i < turnCount; i++) {
+            free(chosenWords[i]);
+        }
+        free(chosenWords);
+        free(words);
+        return 1;
+    }
+     //strcpy(chosenWords[turnCount], retaliate);
+        chosenWords[turnCount] = strdup(retaliate);
         turnCount++;
         if (strcmp(Playing, player1))
             strcpy(Playing, player2);
         else 
             strcpy(Playing, player1);
         
-    } while (isFound(retaliate, words, wordCount) && wordsStartingWithChar(words, retaliate[0], wordCount) && sameLetter(chosenWords[turnCount - 2], retaliate));
+    } while (isFound(retaliate, words, wordCount) && wordsStartingWithChar(words, retaliate[0], wordCount) && (turnCount < 2 || sameLetter(chosenWords[turnCount - 2], retaliate)));
     if (isFound(retaliate, words, wordCount) == 0)
         printf("%s casts a spell unknown! Such a wonky move! %s loses!", Playing, Playing);
     else if (isFound(retaliate, chosenWords, wordCount) == 0)
@@ -154,4 +172,14 @@ int main() {
         printf("it seems the odds were against you, %s. The english lexicon has betrayed thee.", Playing);
     else if (sameLetter(chosenWords[turnCount - 2], retaliate) == 0)
         printf("%s offend the laws of the duel! Read for Merlin and Morgana's sake!", Playing);
+    for (int i = 0; i < turnCount; i++) {
+        free(chosenWords[i]);
+    }
+    free(chosenWords);
+    for (int i = 0; i < wordCount; i++) {
+        free(words[i]);
+    }
+    free(words);
+
+    return 0;
 }
